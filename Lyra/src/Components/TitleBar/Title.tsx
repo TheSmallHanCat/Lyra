862aba6720f541713ac23ddc04d6364c4649b56b
Isogelun <isogelun@gmail.com>
2025-09-29 18:26:04 +0800

    菜单栏但是没有交互

---
+++ Lyra/src/Components/TitleBar/Title.tsx
@@ -0,0 +1,14 @@
+import './TitleBar.css';
+
+const Title = () => {
+
+    return (
+        <div className="title-bar">
+            <div className="title-bar-drag-region">
+                <div className="title">Lyra</div>
+            </div>
+        </div>
+    );
+};
+
+export default Title;
