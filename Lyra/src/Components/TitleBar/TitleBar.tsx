b9948148d21b8cbc136973f2459cf2738902e836
Isogelun <isogelun@gmail.com>
2025-09-29 18:33:29 +0800

    Revert "菜单栏但是没有交互"
    This reverts commit 862aba6720f541713ac23ddc04d6364c4649b56b.

--- Lyra/src/Components/TitleBar/TitleBar.tsx
+++ Lyra/src/Components/TitleBar/TitleBar.tsx
@@ -1,20 +1,37 @@
-﻿import "./TitleBar.css";
-import Title from "./Title.tsx";
-import EditorControlButtons from "./ControlButtons.tsx";
-import EditorMenu from "./Menu.tsx";
+﻿import './TitleBar.css';
 
 const TitleBar = () => {
+    const handleMinimize = () => {
+        window.electron?.minimizeWindow();
+    };
+
+    const handleMaximize = () => {
+        window.electron?.maximizeWindow();
+    };
+
+    const handleClose = () => {
+        window.electron?.closeWindow();
+    };
+
     return (
-        <div className="title-bar-wrapper">
-            <EditorMenu/>
-            <div className="overlay-content">
-                <Title/>
-                <div className="editor-controls-overlay">
-                    <EditorControlButtons/>
-                </div>
+        <div className="title-bar">
+            <div className="title-bar-drag-region">
+                <div className="title">我的应用</div>
+            </div>
+
+            <div className="window-controls">
+                <button className="control-button minimize-btn" onClick={handleMinimize}>
+                    −
+                </button>
+                <button className="control-button maximize-btn" onClick={handleMaximize}>
+                    □
+                </button>
+                <button className="control-button close-btn" onClick={handleClose}>
+                    ×
+                </button>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default TitleBar;
